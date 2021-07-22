454cd3ff3e0ef2d7f29809ddd317a5dd51b024b0
kostekIV <koscis.j@gmail.com>
2021-07-22 13:55:46 +0000

    fix.

--- src/hash/simhash_wrapper.jl
+++ src/hash/simhash_wrapper.jl
@@ -15,16 +15,17 @@ import ..LSH
 
 
 struct SimhasherWrapper{A<:AbstractVector{<:Number}} <: AbstractHasher{A}
-    signature_length::UInt8
+    signature_len::UInt8
     hasher::SimHasher
 
     function SimhasherWrapper{A}(
-        signature_length::UInt8,
+        signature_len::UInt8,
         hasher::SimHasher,
     ) where {A<:AbstractVector{<:Number}}
-        signature_length >= 8 * sizeof(Int) &&
-            error("Signature length can't be greater than 64, got $signature_length")
-        new(signature_length, hasher)
+        signature_len >= 8 * sizeof(Int) && error(
+            "Signature length can't be greater than $(8 * sizeof(Int)), got $signature_len",
+        )
+        new(signature_len, hasher)
     end
 end
 
@@ -34,7 +35,7 @@ function LSH.compute_signatures(
     elem::A,
 )::Vector{Int} where {A<:AbstractVector{<:Number}}
     raw_signature = signature(h.hasher, elem)
-    raw_signature_chunks = partition(raw_signature, h.signature_length)
+    raw_signature_chunks = partition(raw_signature, h.signature_len)
     map(raw_signature_chunks) do boolarray
         BitArray(boolarray).chunks[1]
     end
@@ -53,7 +54,7 @@ const LshSimHash{Id} = Lsh{Vector{Float32},Id,SimhasherWrapper{Vector{Float32}}}
 struct LshSimHashParams
     lsh_params::LshParams
     vector_len::Int
-    signature_length::Int
+    signature_len::Int
     sample_size::Int
 end
 
@@ -73,7 +74,7 @@ function Hash.init_lsh!(
     lsh_params = sim_params.lsh_params
     hasher = initialize!(
         rng,
-        lsh_params.n_tables * sim_params.signature_length,
+        lsh_params.n_tables * sim_params.signature_len,
         sim_params.sample_size,
         sim_params.vector_len,
     )
@@ -82,7 +83,7 @@ function Hash.init_lsh!(
         lsh_params.n_tables,
         lsh_params.n_buckets,
         lsh_params.max_bucket_len,
-        SimhasherWrapper{Vector{Float32}}(UInt8(sim_params.signature_length), hasher),
+        SimhasherWrapper{Vector{Float32}}(UInt8(sim_params.signature_len), hasher),
         Vector{Float32},
         Id,
     )
