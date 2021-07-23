b80d9504a4ff47bb856f62f034ffa0daf639ff46
kostekIV <27210860+kostekIV@users.noreply.github.com>
2021-07-23 13:42:28 +0100

    Glue SimHash to Lsh. (#9)
    Co-authored-by: Casey <caseychandlerh@yahoo.com>

--- src/Slide.jl
+++ src/Slide.jl
@@ -2,7 +2,7 @@ module Slide
 
 include("flux/Flux.jl")
 include("network/Network.jl")
-include("simhash/simhash.jl")
-include("lsh/Lsh.jl")
+
+include("hash/Hash.jl")
 
 end # Slide
---
+++ src/hash/Hash.jl
@@ -0,0 +1,24 @@
+include("./lsh/Lsh.jl")
+include("./simhash/Simhash.jl")
+
+module Hash
+
+export LshParams, init_lsh, init_lsh!
+
+using Base: @kwdef
+
+"""
+Default values are just for convenience.
+"""
+@kwdef struct LshParams
+    n_buckets::Int = 10
+    max_bucket_len::Int = 100
+    n_tables::Int = 100
+end
+
+init_lsh() = error("unimplemented")
+init_lsh!() = error("unimplemented")
+
+end # Hash
+
+include("./simhash_wrapper.jl")
