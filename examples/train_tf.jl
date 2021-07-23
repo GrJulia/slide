c862b52b2f81d35fb4a1a1f48ed490bfbc241e0d
Simon Cowen <simoncowen88@users.noreply.github.com>
2021-07-23 15:53:45 +0100

    working keras code

--- examples/train_tf.jl
+++ examples/train_tf.jl
@@ -1,46 +1,46 @@
 using PyCall
-using JSON
 using Random
+using DataSets
+using Conda
 
-# using Slide.TensorFlowTraining
+Conda.add("tensorflow")
 
-nn = pyimport("tensorflow.nn")
-keras = pyimport("tensorflow.keras")
-
-
-config = JSON.parsefile(ARGS[1])
 
 py"""
 import tensorflow.keras as keras
 import tensorflow.nn as nn
 import numpy as np
 import os
+import time
+from collections import defaultdict
+import json
+
 
 class Model(keras.Model):
     def __init__(self, n_features, hidden_dim, n_classes):
         super(Model, self).__init__()
         self.model = keras.Sequential([
             keras.layers.InputLayer(n_features),
-            keras.layers.Dense(hidden_dim),
+            keras.layers.Dense(hidden_dim, activation='relu'),
             keras.layers.Dense(n_classes),
         ])
 
     def call(self, x):
         return self.model(x)
 
+
 class SparseDataset(keras.utils.Sequence):
-    def __init__(self, ds_path, batch_size, n_features, n_classes):
+    def __init__(self, ds, batch_size, n_features, n_classes):
         super(SparseDataset, self).__init__()
         self.n_features = n_features
         self.n_classes = n_classes
         self.batch_size = batch_size
 
-        self.__preprocess_dataset(ds_path)
+        self.__preprocess_dataset(ds)
 
-    def __preprocess_dataset(self, dataset_path):
-        f = open(dataset_path, "r")
+    def __preprocess_dataset(self, dataset):
         x_indices, x_vals, ys = [], [], []
-        for line in f.readlines()[1:]:
+        for line in dataset.split('\n')[1:-1]:
             line_split = line.split()
             x = list(map(lambda ftr: (int(ftr.split(':')[0]), float(ftr.split(':')[1])), line_split[1:]))
             y = [int(yi) for yi in line_split[0].split(',')]
@@ -73,6 +73,7 @@ class SparseDataset(keras.utils.Sequence):
         
         return x, y
 
+
 class TestAccCallback(keras.callbacks.Callback):
     def __init__(self, test_dataloader, config):
         super().__init__()
@@ -82,106 +83,87 @@ class TestAccCallback(keras.callbacks.Callback):
         self.use_random_indices = config["use_random_indices"]
         self.cnt = 0
 
-    def on_train_batch_end(self, i, batch_logs):
+    def on_train_batch_end(self, batch, logs):
         self.cnt += 1
         if self.cnt % self.freq == 0:
-            self.cnt = 0
-            self.test_epoch()
+            logs["test_acc"] = (self.cnt, self.test_epoch())
 
     def test_epoch(self):
         if self.use_random_indices:
             rand_indices = np.random.randint(0, self.test_dataloader.__len__(), self.n_batches)
         else:
             rand_indices = list(range(0, self.n_batches))
-        batch_size = self.test_dataloader.batch_size
-        acc = 0
+        total_acc, batch_size = 0, self.test_dataloader.batch_size
         for idx in rand_indices:
             x, y = self.test_dataloader.__getitem__(idx)
             out = self.model(x)
             for b in range(batch_size):
                 top_class = np.argmax(out[b])
-                acc += y[b, top_class]
-        print("Accuracy=", acc/(self.n_batches*batch_size)*100)
+                total_acc += y[b, top_class]
+        test_acc = total_acc / (self.n_batches * batch_size)
+        print("test_acc=", test_acc)
+        return test_acc
 
-# def train(config):
-#     model = Model(config["n_features"], config["hidden_dim"], config["n_classes"])
-#     model.compile(optimizer=keras.optimizers.Adam(config["lr"]), loss=lambda y, x: nn.softmax_cross_entropy_with_logits(logits=x, labels=y), run_eagerly=True)
-#     train_set = SparseDataset(config["dataset"]["train_path"], 128, config["n_features"], config["n_classes"])
-#     test_set = SparseDataset(config["dataset"]["test_path"], 128, config["n_features"], config["n_classes"])
-#     test_cb = TestAccCallback(test_set, config["testing"])
-#     tensorboard_cb = keras.callbacks.TensorBoard(log_dir=os.path.join(config["logging_path"], config["name"]))
-#     model.fit(train_set, epochs=config["n_epochs"], callbacks=[test_cb, tensorboard_cb])
+
+class TimeMeasureCallback(keras.callbacks.Callback):
+    def __init__(self):
+        super().__init__()
+        self.cnt = 0
+
+    def on_train_batch_begin(self, batch, logs):
+        self.t0 = time.perf_counter()            
+
+    def on_train_batch_end(self, batch, logs):
+        logs["train_step time"] = (self.cnt, time.perf_counter() - self.t0)
+
+
+class LoggerCallback(keras.callbacks.Callback):
+    def __init__(self, log_dir):
+        super().__init__()
+        self.log_path = os.path.join(log_dir, "logs.json")
+        self.cnt = 0
+        self.logs = defaultdict(lambda: [])       
+
+    def on_train_batch_end(self, batch, batch_logs):
+        self.cnt += 1
+        for key, val in batch_logs.items():
+            if isinstance(val, tuple):
+                self.logs[key].append(val)
+            else:
+                self.logs[key].append((self.cnt, val))
+
+        if self.cnt % 20 == 0:
+            self.save()
+
+    def save(self):
+        with open(self.log_path, "w") as f:
+            json.dump(self.logs, f, indent=4)
+
+
+def train(config):
+    model = Model(config["n_features"], config["hidden_dim"], config["n_classes"])
+    model.compile(optimizer=keras.optimizers.Adam(config["lr"]), loss=nn.softmax_cross_entropy_with_logits, run_eagerly=True)
+
+    train_set = SparseDataset(train_f, config["batch_size"], config["n_features"], config["n_classes"])
+    test_set = SparseDataset(test_f, config["batch_size"], config["n_features"], config["n_classes"])
+
+    log_dir = os.path.join(config["logging_path"], config["name"])
+
+    tensorboard_cb = keras.callbacks.TensorBoard(log_dir=log_dir)
+    test_cb = TestAccCallback(test_set, config["testing"])
+    time_measure_cb = TimeMeasureCallback()
+    logger_cb = LoggerCallback(log_dir)
+    callbacks = [tensorboard_cb, test_cb, time_measure_cb, logger_cb]
+
+    model.fit(train_set, epochs=config["n_epochs"], callbacks=callbacks)
 """
 
-# @pydef mutable struct Model <: keras.Model
-#     function __init__(self, n_features, hidden_dim, n_classes)
-#         __init__(self) = pybuiltin(:super)(Model, self)[:__init__]()
-#         self.model =  keras.Sequential([
-#             keras.layers.Input(n_features),
-#             keras.layers.Dense(hidden_dim),
-#             keras.layers.Dense(n_classes),
-#             keras.layers.Softmax()
-#         ])
-#     end
 
-#     function call(self, x)
-#         println(size(x))
-#         return self.model(x)
-#     end
-# end
+config = JSON.parsefile(ARGS[1])
+config["name"] *= "_" * randstring(8)
+println("Name: $(config["name"])")
 
-# model = keras.Sequential([
-#     keras.layers.InputLayer(config["n_features"]),
-#     keras.layers.Dense(config["hidden_dim"]),
-#     keras.layers.Dense(config["n_classes"]),
-#     keras.layers.Softmax()
-# ])
+train_f = open(String, dataset(config["dataset"]["train_path"]))
+test_f = open(String, dataset(config["dataset"]["test_path"]))
 
-
-# @pydef mutable struct TestAccCallback <: keras.callbacks.Callback
-#     function __init__(self, test_dataloader, freq, n_batches)
-#         __init__(self) = pybuiltin(:super)()[:__init__]()
-#         self.test_dataloader = test_dataloader
-#         self.freq = freq
-#         self.n_batches = n_batches
-#         self.cnt = 0
-#     end
-
-#     function on_train_batch_end(self, i, batch_logs)
-#         self.cnt += 1
-#         if self.cnt % self.freq == 0
-#             self.cnt = 0
-#             self.test_epoch()
-#         end
-#     end
-
-#     function test_epoch(self)
-#         rand_indices = randperm(self.n_batches)
-#         acc = 0
-#         for idx in rand_indices
-#             x, y = self.test_dataloader.__getitem__(idx)
-#             println(size(x), size(y))
-#             out = self.model(x)
-#             println(out[1:5])
-#             for b in 1:128
-#                 top_k_classes = argmax(out[b, :])
-#                 acc += y[b, top_k_classes]
-#             end
-#         end
-#         println("Accuracy=$(acc/self.n_batches*128)")
-#     end
-# end
-
-# py"train"(config)
-
-model = py"Model"(config["n_features"], config["hidden_dim"], config["n_classes"])
-
-model.compile(optimizer=keras.optimizers.Adam(config["lr"]), loss=(y, x) -> nn.softmax_cross_entropy_with_logits(y, x), run_eagerly=true)
-
-train_set = py"SparseDataset"(config["dataset"]["train_path"], 128, config["n_features"], config["n_classes"])
-test_set = py"SparseDataset"(config["dataset"]["test_path"], 128, config["n_features"], config["n_classes"])
-
-test_cb = py"TestAccCallback"(test_set, config["testing"])
-tensorboard_cb = keras.callbacks.TensorBoard(log_dir=joinpath(config["logging_path"], config["name"]))
-
-model.fit(train_set, epochs=config["n_epochs"], callbacks=[test_cb, tensorboard_cb])
+py"train"(config, train_f, test_f)
