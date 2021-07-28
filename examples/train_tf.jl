f5674c4307ad0cb42762b2f0197f10aecddf9f2e
Kostek Subbotko <ks395088@students.mimuw.edu.pl>
2021-07-28 12:29:53 +0000

    fixed a few minor things

--- examples/train_tf.jl
+++ examples/train_tf.jl
@@ -11,11 +11,11 @@ pushfirst!(PyVector(pyimport("sys")."path"), "/mnt/data/code/slide/src/tf")
 train = pyimport("train")
 
 
-config = JSON.parsefile("examples/configs/default_amazon.json")
+config = JSON.parsefile(ARGS[1])
 config["name"] *= "_tf_" * randstring(8)
 println("Name: $(config["name"])")
 
 train_f = open(String, dataset(config["dataset"]["train_path"]))
 test_f = open(String, dataset(config["dataset"]["test_path"]))
 
-train(config, train_f, test_f)
+train.train(config, train_f, test_f)
