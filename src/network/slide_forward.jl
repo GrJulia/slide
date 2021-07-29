130a7011c5ad3230a1ed7d3cd66f3dff97d51729
MaxenceGelard <maxence.gelard@student-cs.fr>
2021-07-29 15:51:15 +0100

    Move hashing to forward loop

--- src/network/slide_forward.jl
+++ src/network/slide_forward.jl
@@ -2,36 +2,39 @@ using LinearAlgebra
 
 function build_activated_neurons_single_sample(
     x::SubArray{Float},
-    network::SlideNetwork,
+    layer::Layer,
     random::Bool,
-)::Vector{Vector{Int}}
-    activated_neuron_ids = []
-    for layer in network.layers
-        current_hash_table = layer.hash_table
-        input_hash =
-            random ? get_random_hash(current_hash_table, x) :
-            get_deterministic_hash(current_hash_table, x)
-        neuron_ids = retrieve_ids_from_bucket(current_hash_table, input_hash)
-        push!(activated_neuron_ids, neuron_ids)
-    end
-    return activated_neuron_ids
+)::Vector{Int}
+    current_hash_table = layer.hash_table
+    input_hash =
+        random ? get_random_hash(current_hash_table, x) :
+        get_deterministic_hash(current_hash_table, x)
+    return retrieve_ids_from_bucket(current_hash_table, input_hash)
 end
 
 function forward_single_sample(
     x::SubArray{Float},
     network::SlideNetwork,
-    activated_neuron_ids::Vector,
     x_index::Int,
-)::Vector{Float}
+    random::Bool,
+)::Tuple{Vector{Float},Vector{Id}}
     n_layers = length(network.layers)
     current_input = x
+    activated_neuron_ids = nothing
     for i = 1:n_layers
         # compute activated neurons with current_input
         layer = network.layers[i]
+
+        activated_neuron_ids =
+            build_activated_neurons_single_sample((@view current_input[:]), layer, random)
+        for neuron_id in activated_neuron_ids
+            layer.neurons[neuron_id].active_inputs[x_index] = 1
+        end
+
         current_n_neurons = length(layer.neurons)
         layer_activation = layer.layer_activation
         layer_output = zeros(current_n_neurons)
-        for neuron_id in activated_neuron_ids[i]
+        for neuron_id in activated_neuron_ids
             current_neuron = layer.neurons[neuron_id]
             layer_output[neuron_id] =
                 dot(current_input, current_neuron.weight) + current_neuron.bias
@@ -42,18 +45,7 @@ function forward_single_sample(
             neuron.activation_inputs[x_index] = current_input[k]
         end
     end
-    return current_input
-end
-
-function handle_batch(x::SubArray{Float}, network::SlideNetwork, i::Int, random)
-    activated_neuron_ids = build_activated_neurons_single_sample(x, network, random)
-    for j = 1:length(activated_neuron_ids)
-        for neuron_id in activated_neuron_ids[j]
-            network.layers[j].neurons[neuron_id].active_inputs[i] = 1
-        end
-    end # need to be done in forward_single_sample function at each iteration, using current_input
-    return forward_single_sample(x, network, activated_neuron_ids, i),
-    activated_neuron_ids[end]
+    return current_input, activated_neuron_ids
 end
 
 function forward!(x::Array{Float}, network::SlideNetwork, random::Bool = false)
@@ -62,7 +54,7 @@ function forward!(x::Array{Float}, network::SlideNetwork, random::Bool = false)
     last_layer_activated_neuron_ids = Vector{Vector{Id}}(undef, n_samples)
     Threads.@threads for i = 1:n_samples
         output[:, i], last_layer_activated_neuron_ids_batch =
-            handle_batch((@view x[:, i]), network, i, random)
+            forward_single_sample((@view x[:, i]), network, i, random)
         last_layer_activated_neuron_ids[i] = last_layer_activated_neuron_ids_batch
     end
     output, last_layer_activated_neuron_ids
