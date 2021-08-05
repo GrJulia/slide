d587136d05714a0250b23af773a05e8b1707d86f
Kostek Subbotko <ks395088@students.mimuw.edu.pl>
2021-08-05 17:33:28 +0000

    sparse scripts works

--- src/tf/train_sparse.py
+++ src/tf/train_sparse.py
@@ -7,6 +7,7 @@ import time
 from collections import defaultdict
 import json
 import math
+import functools
 
 
 class SparseDense(keras.layers.Layer):
@@ -33,61 +34,46 @@ class Model(keras.Model):
         return self.model(x)
 
 
-class SparseDataset(keras.utils.Sequence):
-    def __init__(self, ds, batch_size, n_features, n_classes):
-        super(SparseDataset, self).__init__()
-        self.n_features = n_features
-        self.n_classes = n_classes
-        self.batch_size = batch_size
+def gen_sparse_dataset(dataset, batch_size, n_features, n_classes, n_samples=None):
+    x_indices, x_vals, raw_ys = [], [], []
+    cnt = 0
+    for line in dataset.split('\n')[1:-1]:
+        line_split = line.split()
+        x = [(int(ftr.split(':')[0]), float(ftr.split(':')[1])) for ftr in line_split[1:]]
+        y = [int(yi) for yi in line_split[0].split(',')]
+        x_indices.append(np.array([xi[0] for xi in x]))
+        x_vals.append([xi[1] for xi in x])
+        raw_ys.append(np.array(y))
+        cnt += 1
+        if n_samples is not None and cnt == batch_size * n_samples:
+            break
 
-        self._preprocess_dataset(ds)
+    perm = np.random.permutation(len(raw_ys))
+    xs_indices = [x_indices[idx] for idx in perm]
+    xs_vals = [x_vals[idx] for idx in perm]
+    ys = [raw_ys[idx] for idx in perm]
 
-    def _preprocess_dataset(self, dataset):
-        x_indices, x_vals, ys = [], [], []
-        for line in dataset.split('\n')[1:-1]:
-            line_split = line.split()
-            x = [(int(ftr.split(':')[0]), float(ftr.split(':')[1])) for ftr in line_split[1:]]
-            y = [int(yi) for yi in line_split[0].split(',')]
-            x_indices.append(np.array([xi[0] for xi in x]))
-            x_vals.append([xi[1] for xi in x])
-            ys.append(np.array(y))
-    
-        perm = np.random.permutation(len(ys))
-        p_x_indices = [x_indices[idx] for idx in perm]
-        p_x_vals = [x_vals[idx] for idx in perm]
-        p_ys = [ys[idx] for idx in perm]
-    
-        self.xs = (p_x_indices, p_x_vals)
-        self.ys = p_ys
-
-    def __len__(self):
-        return len(self.ys) // self.batch_size
-
-    def __getitem__(self, b_idx):
-        (xs_indices, xs_vals) = self.xs
-
+    for b_idx in range(len(ys)//batch_size):
         x_indices, x_vals = [], []
-        y = np.zeros((self.batch_size, self.n_classes))
-        for idx in range(self.batch_size):
-            tr_idx = b_idx * self.batch_size + idx
+        y = np.zeros((batch_size, n_classes))
+        for idx in range(batch_size):
+            tr_idx = b_idx * batch_size + idx
 
             x_indices += [[idx, x_index] for x_index in xs_indices[tr_idx]]
             x_vals += xs_vals[tr_idx]
 
-            ys = self.ys[tr_idx]
-            y[idx, ys] = 1
+            y[idx, ys[tr_idx]] = 1
 
-        x = tf.SparseTensor(x_indices, x_vals, (self.batch_size, self.n_features))
-        return x, y
+        yield (np.array(x_indices), np.array(x_vals), np.array([batch_size, n_features]), y)
 
 
 class TestAccCallback(keras.callbacks.Callback):
-    def __init__(self, test_set, config):
+    def __init__(self, test_set, config, batch_size):
         super().__init__()
         self.test_set = test_set
         self.freq = config["test_freq"]
         self.n_batches = config["n_batches"]
-        self.use_random_indices = config["use_random_indices"]
+        self.batch_size = batch_size
         self.cnt = 0
 
     def on_train_batch_end(self, batch, logs):
@@ -96,16 +82,14 @@ class TestAccCallback(keras.callbacks.Callback):
             logs["test_acc"] = (self.cnt, self.test_epoch())
 
     def test_epoch(self):
-        if self.use_random_indices:
-            rand_indices = np.random.randint(0, len(self.test_set), self.n_batches)
-        else:
-            rand_indices = np.arange(self.n_batches)
-        total_acc, batch_size = 0, self.test_set.batch_size
-        for idx in rand_indices:
-            x, y = self.test_set[idx]
+        total_acc, batch_size = 0, self.batch_size
+        cnt = 0
+        for x, y in self.test_set:
             out = self.model(x)
             top_class = np.argmax(out, axis=1).reshape(batch_size, 1)
-            total_acc += np.sum(np.take_along_axis(y, top_class, axis=1)) / batch_size
+            total_acc += np.sum(np.take_along_axis(np.array(y), top_class, axis=1)) / batch_size
+            cnt += 1
+        assert(cnt == self.n_batches)
         test_acc = total_acc / self.n_batches
         return test_acc
 
@@ -149,13 +133,18 @@ def train(config, train_f, test_f):
     model = Model(config["n_features"], config["hidden_dim"], config["n_classes"])
     model.compile(optimizer=keras.optimizers.Adam(config["lr"]), loss=nn.softmax_cross_entropy_with_logits, run_eagerly=True)
 
-    train_set = SparseDataset(train_f, config["batch_size"], config["n_features"], config["n_classes"])
-    test_set = SparseDataset(test_f, config["batch_size"], config["n_features"], config["n_classes"])
+    train_gen_with_args = functools.partial(gen_sparse_dataset, train_f, config["batch_size"], config["n_features"], config["n_classes"])
+    test_gen_with_args = functools.partial(gen_sparse_dataset, test_f, config["batch_size"], config["n_features"], config["n_classes"], n_samples=config["testing"]["n_batches"])
+
+    train_set = (tf.data.Dataset.from_generator(train_gen_with_args, (tf.int64, tf.float32, tf.int64, tf.float32))
+                .map(lambda i, v, s, y: (tf.SparseTensor(i, v, s), y)))
+    test_set = (tf.data.Dataset.from_generator(test_gen_with_args, (tf.int64, tf.float32, tf.int64, tf.float32))
+                .map(lambda i, v, s, y: (tf.SparseTensor(i, v, s), y)))
 
     log_dir = os.path.join(config["logging_path"], config["name"])
 
     tensorboard_cb = keras.callbacks.TensorBoard(log_dir=log_dir)
-    test_cb = TestAccCallback(test_set, config["testing"])
+    test_cb = TestAccCallback(test_set, config["testing"], config["batch_size"])
     time_measure_cb = TimeMeasureCallback()
     logger_cb = LoggerCallback(log_dir)
     callbacks = [tensorboard_cb, time_measure_cb, test_cb, logger_cb]
