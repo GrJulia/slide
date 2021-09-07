853c8cb2c704a3d538f0c9c0e1cbb95764e6b349
kostekIV <27210860+kostekIV@users.noreply.github.com>
2021-09-07 09:10:11 +0100

    Fix stack overflow. (#47)

--- src/logger/slide_logger.jl
+++ src/logger/slide_logger.jl
@@ -1,7 +1,9 @@
-using TensorBoardLogger
 import Logging
-using DataStructures
+
 using JSON
+using Logging: LogLevel, Info
+using TensorBoardLogger: TBLogger
+using DataStructures: DefaultDict
 
 
 mutable struct SlideLogger <: Logging.AbstractLogger
@@ -10,14 +12,21 @@ mutable struct SlideLogger <: Logging.AbstractLogger
     tb_logger::Union{TBLogger,Nothing}
     log_path::String
     incr_tb::Bool
+    min_level::LogLevel
 
-    SlideLogger(tb_logger, log_dir) =
-        new(0, DefaultDict(() -> []), tb_logger, joinpath(log_dir, "logs.json"), false)
+    SlideLogger(tb_logger, log_dir, level = Info) = new(
+        0,
+        DefaultDict(() -> []),
+        tb_logger,
+        joinpath(log_dir, "logs.json"),
+        false,
+        level,
+    )
 end
 
 Logging.handle_message(
     logger::SlideLogger,
-    level::Logging.LogLevel,
+    level::LogLevel,
     message::Nothing,
     args...;
     kwargs...,
@@ -25,7 +34,7 @@ Logging.handle_message(
 
 function Logging.handle_message(
     logger::SlideLogger,
-    level::Logging.LogLevel,
+    level::LogLevel,
     message::String,
     args...;
     kwargs...,
@@ -52,8 +61,7 @@ end
 
 Logging.shouldlog(::SlideLogger, args...) = true
 
-Logging.min_enabled_level(::SlideLogger) =
-    Logging.min_enabled_level(Logging.current_logger())
+Logging.min_enabled_level(logger::SlideLogger) = logger.min_level
 
 function step!(logger::SlideLogger)
     logger.curr_it += 1
@@ -86,11 +94,11 @@ function save(logger::SlideLogger)
     end
 end
 
-function get_logger(config, name)
+function get_logger(config, name, log_level = Info)
     log_dir = config["logging_path"] * "/" * name
     mkpath(log_dir)
 
     tb_logger = config["use_tensorboard"] ? TBLogger(log_dir) : nothing
 
-    SlideLogger(tb_logger, log_dir)
+    SlideLogger(tb_logger, log_dir, log_level)
 end
